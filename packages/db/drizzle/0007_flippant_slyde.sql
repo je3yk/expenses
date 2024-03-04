1d8c54ac4efa2c4b0d76c072da8eaa19b4084e05
jedrzej.zawojski <jedrzej.zawojski95@gmail.com>
2024-03-04 19:39:00 +0100

    fix: Fix column types and input fields typing

---
+++ packages/db/drizzle/0007_flippant_slyde.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "users" ALTER COLUMN "id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "created_at" SET DATA TYPE timestamp with time zone;--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "created_at" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "updated_at" SET DATA TYPE timestamp with time zone;
