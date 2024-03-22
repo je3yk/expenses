80d62f882086a3edd52b03db12d7259e5841ce50
jedrzej.zawojski <jedrzej.zawojski95@gmail.com>
2024-03-22 19:40:43 +0100

    Add baseUrl fetching

--- apps/web/src/providers/trpc-provider.tsx
+++ apps/web/src/providers/trpc-provider.tsx
@@ -7,6 +7,22 @@ import superjson from "superjson";
 
 import { trpc } from "~/trpc/client";
 
+function getBaseUrl() {
+  if (typeof window !== "undefined") {
+    return "";
+  }
+
+  if (process.env.VERCEL_URL) {
+    return `$https://${process.env.VERCEL_URL}`;
+  }
+
+  if (process.env.RENDER_INTERNAL_HOSTNAME) {
+    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}`;
+  }
+
+  return `http://localhost:${process.env.PORT || 3000}`;
+}
+
 export default function TrpcProvider({
   children,
 }: {
@@ -17,7 +33,7 @@ export default function TrpcProvider({
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
       transformer: superjson,
